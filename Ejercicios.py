5d0bafa29d1bd391cb135f9519a712540e9c3baf
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2019-09-29 22:31:36 +0200

    ACABAR

---
+++ Ejercicios.py
@@ -0,0 +1,14 @@
+#7.6.1
+
+tupla1 = (4,7,2,9,11,13,20)
+
+marca1 = 0
+
+#Uso de for en Python: (ver info en libro o internet)
+for elemento in tupla1:
+    if tupla1[elemento] < tupla1[elemento+1]
+
+
+
+
+
