5ac8073c93ed0b5b4f0d9fda17598a437450d83f
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2019-09-30 21:15:14 +0200

    ExerciciosPython ACABAR

--- Ejercicios.py
+++ Ejercicios.py
@@ -3,42 +3,40 @@
 
 tupla1 = (4,7,2,9,11,13,20)
 tupla2 = (2,3,4,5,6,7,8,9)
-marca1 = False
 
 
 #Uso de for en Python: (ver info en libro o internet)
-#no va?
+#no va el Range???
 """for i in Range(0, tupla1.__sizeof__(), 1):
     if tupla1[i] < tupla1[i+1]:
         marca1 = True"""
 
-
-
-
-
-#usando otro tipo de for y funciones:
+#Usando otro tipo de for y funciones:
 def comprobarOrden(tupla):
     marca2 = True
-    anterior = tupla2[0]
-    for elemento in tupla2:
+    anterior = tupla[0]
+    for elemento in tupla:
         if elemento < anterior:
-            print("no ordenado")
-            marca2 = False
 
-            break
+             marca2 = False
+
+             break
 
         anterior = elemento;
-        if marca2:
-            print("ordenada de menor a mayor")
-
-
+    if marca2 == True:
+         print("ordenada de menor a mayor")
+    else:
+         print("no ordenado")
 
 comprobarOrden(tupla2)
+
+
+
 
 #7.6.2.1
 
 #duplas del domino:
-
+#si, por ejemplo, las duplas tuvieran más valores, podríamos hacer un for "a lo java" para que recorriese to' automáticamente
 def domino(ficha1,ficha2):
     encajan = False
     if ficha1[0] == ficha2[0]:
@@ -51,14 +49,14 @@ def domino(ficha1,ficha2):
         encajan = True
 
     if encajan:
-        print("Las fichas("+str(ficha1[0]) + "," + str(ficha1[1]) + ") ("+ str(ficha2[0]) + " , "+ str(ficha2[1]) + ") encajan")
+        print("Las fichas("+str(ficha1[0]) + "," + str(ficha1[1]) + ") ("+ str(ficha2[0]) + ","+ str(ficha2[1]) + ") encajan")
 
     else:
         print("no encajan")
 
 
-
-domino((1,2),(4,3))
+#Obviamente la función puede recibir duplas directamente o variables de duplas
+domino((1,2),(4,2))
 
 
 #7.6.7
@@ -101,9 +99,12 @@ campanhaElectoral(("Juan", "Pepe", "Pedro"),0,1)
 
 #7.6.3.3 ACABAR
 
-def campanhaElectoral (nombres, origen, num):
-    for elemento in nombres[origen:num+1]:
-        print("Estimado" + elemento[0])
+def campanhaElectoral (nombres):
+    for elemento in nombres:
+        if elemento[1] == 'M':
+            print("Estimado" + elemento[0])
 
-campanhaElectoral(("Juan", "Pepe", "Pedro"),0,1)
 
+campanhaElectoral(("Juan",'M'), ("Pepe","M"), ("Maria","F"))
+
+#7.6.5 ACABAR
