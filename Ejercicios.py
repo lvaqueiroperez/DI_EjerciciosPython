05ef3ca5cc77bac1addb85e39cd796c8d9bd6356
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2019-10-03 11:04:42 +0200

    Exercicios Python

--- Ejercicios.py
+++ Ejercicios.py
@@ -114,33 +114,84 @@ def campanhaElectoral (nombres):
 
 
 #9.5.1
+#(podemos hacerlo de la manera fácil, donde no se repiten claves)
 #LISTA de tuplas a Diccionario
 #Las tuplas de la lista solo podrán tener 2 elementos, lo que será la clave y el valor de cada elemento del diccionario
 
 #Para poner varios valores en una misma clave, creamos una lista la lista como Valor
 tuplasAlmacen = [("1","Clavo"),("2","Tuerca"),("1","Tornillo"),("3","Tenazas")]
 
-def tuplasADiccionario(lista):
+def tuplasADiccionario(listaT):
 
         diccionario = dict()
-        listaValoresTemporal = list()
-        listaValores2 = list()
 
-        for tupla in lista:
-
-            for tupla2 in lista:
-
-                if tupla[0] == tupla2[0]:
-                    #siempre almacena el primer valor independientemente de que se repita o no
-                    listaValoresTemporal.extend(tupla2[0])
-
-            listaValores2.extend(listaValoresTemporal)
-            listaValoresTemporal.clear()
-            diccionario[tupla] = listaValores2
+        for tupla in listaT:
+            if tupla[0] in diccionario:
+                #Si el valor existe en el diccionario, haremos una lista y utilizaremos "append" para añadirle un elemento
+                #Aquí tenemos que ASUMIR que esa lista ya existe
+                diccionario[tupla[0]].append(tupla[1])
+            else:
+                #Todos los valores serán una lista, que creamos aquí
+                lista2 = list()
+                lista2.append(tupla[1])
+                diccionario[tupla[0]] = lista2
 
         return diccionario
 
 print(tuplasADiccionario(tuplasAlmacen))
+
+
+#9.5.2 !!!
+#Podemos pasar un String a Mayus o Minus con "upper" y "lower" para evitar diferenciación de mayus y minus
+cad = "Qué lindo día que hace hoy"
+
+def contarPalabras(cadena):
+
+    diccionario2 = dict()
+    listaPalabras = cadena.split(" ")
+
+    for palabra in listaPalabras:
+
+        if palabra in diccionario2:
+
+            diccionario2[palabra] = diccionario2[palabra] + 1
+
+        else:
+
+            diccionario2[palabra] = 1
+
+    return diccionario2
+
+print(contarPalabras(cad))
+
+#9.5.2.2
+"""Escribir una función que cuente la cantidad de apariciones de cada carácter en una
+cadena de texto, y los devuelva en un diccionario"""
+
+cad2 = "Esto es una cadena de texto"
+
+#EN PYTHON, LOS STRING CUENTAN COMO COLECCIÓN, SE PUEDEN RECORRER COMO SI FUERAN UNA ESPECIE DE "LISTA DE CARACTERES"
+#Entonces, cogiendo el ejercicio anterior, solo borrando el "listaPalabras = cadena.split(" ")" ya iría
+
+def contarCaracteres(cadena):
+
+    diccionario2 = dict()
+    listaPalabras = cadena
+
+    for palabra in listaPalabras:
+
+        if palabra in diccionario2:
+
+            diccionario2[palabra] = diccionario2[palabra] + 1
+
+        else:
+
+            diccionario2[palabra] = 1
+
+    return diccionario2
+
+print(contarCaracteres(cad2))
+
 
 
 
